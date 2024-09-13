641a228f435cfcc4e5a86b197f9ef786a55a2ec7
MrJ0nik <142501317+MrJ0nik@users.noreply.github.com>
2024-09-13 20:03:38 +0300

    second commit

--- lab 2/lab 2.cpp
+++ lab 2/lab 2.cpp
@@ -10,8 +10,8 @@ int main()
 	double x; // вхідний параметр
 	double z1; // результат обчислення 1-го виразу
 	//double z2; // результат обчислення 2-го виразу
-	//cout << "x = "; cin >> x;
-	z1 = (cos(x) + sin(x)) / (cos(x) - sin(x));
+	cout << "x = "; cin >> x;
+	//z1 = (cos(x) + sin(x)) / (cos(x) - sin(x));
 	//z2 = tan(2*x) + (1 / (cos(2*x));
 	cout << endl;
 	cout << "z1 = " << z1 << endl;
